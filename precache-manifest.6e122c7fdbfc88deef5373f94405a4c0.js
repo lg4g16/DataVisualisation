63f3075aada95bf0f249d6792a8986b5c5f6c5bb
lg4g16 <lg4g16@soton.ac.uk>
2020-01-10 13:23:43 +0000

    Updates

---
+++ precache-manifest.6e122c7fdbfc88deef5373f94405a4c0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b517949264b5f60badc7a3d6ac6b5c9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2f208eee89e2bb27cbbf",
+    "url": "./static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "468c5b4e3fb2e6eccd33",
+    "url": "./static/css/main.97e8edec.chunk.css"
+  },
+  {
+    "revision": "2f208eee89e2bb27cbbf",
+    "url": "./static/js/2.f0e58701.chunk.js"
+  },
+  {
+    "revision": "7ee6a5f7944d7e75a7c68738c5c4005b",
+    "url": "./static/js/2.f0e58701.chunk.js.LICENSE"
+  },
+  {
+    "revision": "468c5b4e3fb2e6eccd33",
+    "url": "./static/js/main.53a929e5.chunk.js"
+  },
+  {
+    "revision": "9f7f031a722dcd45ae62",
+    "url": "./static/js/runtime-main.de284e28.js"
+  },
+  {
+    "revision": "1cf58b915e9f37c63d8876eb037171c2",
+    "url": "./static/media/UkAveragePersonWaste.1cf58b91.png"
+  },
+  {
+    "revision": "95474c5212908d59260d6a1f692faacd",
+    "url": "./static/media/home-icon.95474c52.png"
+  }
+]);
